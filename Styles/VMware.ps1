c9a7f965b985ba61707b2db6e0a9f7bacfe1e86b
Tim Carman <tpcarman@gmail.com>
2018-04-03 23:08:55 +1000

    Updated style colours

--- Styles/VMware.ps1
+++ Styles/VMware.ps1
@@ -1,20 +1,20 @@
 #region VMware Document Style
 DocumentOption -EnableSectionNumbering -PageSize A4 -DefaultFont 'Arial' -MarginLeftAndRight 71 -MarginTopAndBottom 71
 
-Style -Name 'Title' -Size 24 -Color '004B70' -Align Center
-Style -Name 'Title 2' -Size 18 -Color '98441E' -Align Center
-Style -Name 'Title 3' -Size 12 -Color '98441E' -Align Left
-Style -Name 'Heading 1' -Size 16 -Color '98441E' 
-Style -Name 'Heading 2' -Size 14 -Color '98441E' 
-Style -Name 'Heading 3' -Size 12 -Color '98441E' 
-Style -Name 'Heading 4' -Size 11 -Color '98441E' 
+Style -Name 'Title' -Size 24 -Color '002538' -Align Center
+Style -Name 'Title 2' -Size 18 -Color '2C7AC4' -Align Center
+Style -Name 'Title 3' -Size 12 -Color '2C7AC4' -Align Left
+Style -Name 'Heading 1' -Size 16 -Color '2C7AC4' 
+Style -Name 'Heading 2' -Size 14 -Color '2C7AC4' 
+Style -Name 'Heading 3' -Size 12 -Color '2C7AC4' 
+Style -Name 'Heading 4' -Size 11 -Color '2C7AC4' 
 Style -Name 'Heading 5' -Size 10 -Color '565656' -Italic
-Style -Name 'H1 Exclude TOC' -Size 16 -Color '98441E' 
+Style -Name 'H1 Exclude TOC' -Size 16 -Color '2C7AC4' 
 Style -Name 'Normal' -Size 10 -Color '565656' -Default
-Style -Name 'TOC' -Size 16 -Color '98441E' 
+Style -Name 'TOC' -Size 16 -Color '2C7AC4' 
 Style -Name 'TableDefaultHeading' -Size 10 -Color 'FAF7EE' -BackgroundColor '002538' 
 Style -Name 'TableDefaultRow' -Size 10 
-Style -Name 'TableDefaultAltRow' -Size 10 -BackgroundColor 'DDDDDD' 
+Style -Name 'TableDefaultAltRow' -Size 10 -BackgroundColor 'D9E4EA' 
 Style -Name 'Critical' -Size 10 -BackgroundColor 'FFB38F'
 Style -Name 'Warning' -Size 10 -BackgroundColor 'FFE860'
 Style -Name 'Info' -Size 10 -BackgroundColor 'A6D8E7'
