b714ac9fdd34b387bac33747d2459927e893e466
Tim Carman <tpcarman@gmail.com>
2018-06-23 17:25:44 +1000

    Development

--- Reports/New-UCS/New-UCS.ps1
+++ Reports/New-UCS/New-UCS.ps1
@@ -5,8 +5,7 @@
 #                                    CONFIG SETTINGS                                          #
 ###############################################################################################
 $ScriptPath = (Get-Location).Path
-#$ReportConfigFile = Join-Path $ScriptPath $("Reports\$Type\$Type.json")
-$ReportConfigFile = Join-Path $ScriptPath $("Reports\UCS\UCS.json")
+$ReportConfigFile = Join-Path $ScriptPath $("Reports\$Type\$Type.json")
 If (Test-Path $ReportConfigFile -ErrorAction SilentlyContinue) {
     $ReportConfig = Get-Content $ReportConfigFile | ConvertFrom-json
 }
@@ -16,7 +15,7 @@ if (!$StyleName) {
 }
 
 # Connect to Cisco UCS domain using supplied credentials
-$UCSC = Connect-Ucs -Name $Target -Credential $Credentials
+$handle = Connect-Ucs -Name $Target -Credential $Credentials
 #endregion Configuration Settings
 
 #region Script Body
@@ -28,7 +27,33 @@ $UCSC = Connect-Ucs -Name $Target -Credential $Credentials
 Section -Style Heading1 -Name 'System' {
 
     Section -Style Heading2 -Name 'Status' {
-
+        $system = Get-UcsStatus -Ucs $handle #| Select-Object Name, VirtualIpv4Address, HaReady, FiALeadership, FiAManagementServicesState, FiBLeadership, FiBManagementServicesState
+        $a = @{
+            Name         = $system.Name
+            VIP          = $system.VirtualIpv4Address
+            UCSM         = (Get-UcsMgmtController -Ucs $handle -Subject system | Get-UcsFirmwareRunning).Version
+            HaReady      = $system.HaReady
+            #--- Get Full State and Logical backup configuration ---#
+            BackupPolicy = (Get-UcsMgmtBackupPolicy -Ucs $handle | Select AdminState).AdminState
+            ConfigPolicy = (Get-UcsMgmtCfgExportPolicy -Ucs $handle | Select AdminState).AdminState
+            #--- Get Call Home admin state ---#
+            CallHome     = (Get-UcsCallHome -Ucs $handle | Select-Object AdminState).AdminState
+            <#
+            #--- Get System and Server power statistics ---#
+            $DomainHash.System.Chassis_Power = @()
+            $DomainHash.System.Chassis_Power += Get-UcsChassisStats -Ucs $handle | Select-Object Dn, InputPower, InputPowerAvg, InputPowerMax, OutputPower, OutputPowerAvg, OutputPowerMax, Suspect
+            $DomainHash.System.Chassis_Power | % {$_.Dn = $_.Dn -replace ('(sys[/])|([/]stats)', "") }
+            $DomainHash.System.Server_Power = @()
+            $DomainHash.System.Server_Power += Get-UcsComputeMbPowerStats -Ucs $handle | Sort-Object -Property Dn | Select-Object Dn, ConsumedPower, ConsumedPowerAvg, ConsumedPowerMax, InputCurrent, InputCurrentAvg, InputVoltage, InputVoltageAvg, Suspect
+            $DomainHash.System.Server_Power | % {$_.Dn = $_.Dn -replace ('([/]board.*)', "") }
+            #--- Get Server temperatures ---#
+            $DomainHash.System.Server_Temp = @()
+            $DomainHash.System.Server_Temp += Get-UcsComputeMbTempStats -Ucs $handle | Sort-Object -Property Ucs, Dn | Select-Object Dn, FmTempSenIo, FmTempSenIoAvg, FmTempSenIoMax, FmTempSenRear, FmTempSenRearAvg, FmTempSenRearMax, FmTempSenRearL, FmTempSenRearLAvg, FmTempSenRearLMax, FmTempSenRearR, FmTempSenRearRAvg, FmTempSenRearRMax, Suspect
+            $DomainHash.System.Server_Temp | % {$_.Dn = $_.Dn -replace ('([/]board.*)', "") }
+            #>
+        }
+        $t = $a | select vip, ucsm, HaReady
+        $t | table     
     }
 
     Section -Style Heading2 -Name 'Chassis Power Statistics' {
@@ -42,13 +67,12 @@ Section -Style Heading1 -Name 'System' {
     Section -Style Heading2 -Name 'Server Temperature Statistics' {
         
     }
-
 }
 
 Section -Style Heading1 -Name 'Inventory' {
 
     Section -Style Heading2 -Name 'Fabric Interconnects' {
-        
+
     }
 
     Section -Style Heading2 -Name 'Chassis' {
@@ -64,7 +88,13 @@ Section -Style Heading1 -Name 'Inventory' {
     }
 
     Section -Style Heading2 -Name 'Rack Servers' {
-        
+        $UcsRackUnit = Get-UcsRackUnit | Sort-Object ServerID | Select-Object @{L = 'Rack Id'; E = {$_.ServerId}}, Model, Serial, @{L = 'Service Profile'; E = {$_.AssignedToDn}}, @{L = 'CPUs'; E = {$_.NumOfCpus}}, @{L = 'Cores'; E = {$_.NumOfCores}}, Threads
+        <#>
+        @{L = 'Available Memory'; E = {$_.AvailableMemory}}, , `
+                @{L = 'Number of Adapters'; E = {$_.NumOfAdaptors}}, @{L = 'Number of Ethernet Interfaces'; E = {$_.NumOfEthHostIfs}}, @{L = 'Number of FC Host Interfaces'; E = {$_.NumOfFcHostIfs}}, , Presence, @{L = 'Operability'; E = {$_.OperState}}, `
+                    Operability, @{L = 'Power'; E = {$_.OperPower}}, Serial
+                $UcsRackUnit | Table -Name 'Server Inventory'
+                #>
     }
 
     Section -Style Heading2 -Name 'Rack Adapters' {
@@ -168,4 +198,4 @@ Section -Style Heading1 -Name 'Fault Summary' {
 #endregion Script Body
 
 # Disconnect UCS Chassis
-$Null = Disconnect-Ucs -Ucs $Target
+$Null = Disconnect-Ucs -Ucs $handle
