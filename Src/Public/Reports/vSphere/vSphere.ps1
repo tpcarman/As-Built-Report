2e34306b26ed8219d2f18a2bb4ee76e2cb1c5552
Erwan Quélin <erwan.quelin@gmail.com>
2018-10-12 11:30:50 +0200

    Add PCI devices drivers and firmware details for VMhosts (#46)
    Add PCI devices drivers and firmware details for VMhosts.
    
    Addresses issue #19

--- Src/Public/Reports/vSphere/vSphere.ps1
+++ Src/Public/Reports/vSphere/vSphere.ps1
@@ -410,6 +410,106 @@ function Get-ScsiDeviceDetail {
         'CapacityGB' = $CapacityGB
     }
 }
+
+Function Get-PciDeviceDetail {
+    <#
+    .SYNOPSIS
+    Helper function to return PCI Devices Drivers & Firmware information for a specific host.
+    .PARAMETER Server
+    vCenter VISession object.
+    .PARAMETER esxcli
+    Esxcli session object associated to the host.
+    .EXAMPLE
+    $Credentials = Get-Crendentials
+    $Server = Connect-VIServer -Server vcenter01.example.com -Credentials $Credentials
+    $VMHost = Get-VMHost -Server $Server -Name esx01.example.com
+    $esxcli = Get-EsxCli -Server $Server -VMHost $VMhost -V2
+    Get-PciDeviceDetail -Server $vCenter -esxcli $esxcli
+
+    VMkernel Name    : vmhba0
+    Device Name      : Sunrise Point-LP AHCI Controller
+    Driver           : vmw_ahci
+    Driver Version   : 1.0.0-34vmw.650.0.14.5146846
+    Firmware Version : NA
+    VIB Name         : vmw-ahci
+    VIB Version      : 1.0.0-34vmw.650.0.14.5146846
+
+    .NOTES
+    Author: Erwan Quelin heavily based on the work of the vDocumentation team - https://github.com/arielsanchezmora/vDocumentation/blob/master/powershell/vDocumentation/Public/Get-ESXIODevice.ps1
+    #>
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory = $true)]
+        $Server,
+        [Parameter(Mandatory = $true)]
+        $esxcli
+    )
+
+    Begin {}
+    
+    Process {
+
+        # Set default results
+        $firmwareVersion = "N/A"
+        $vibName = "N/A"
+        $driverVib = @{
+            Name = "N/A"
+            Version = "N/A"
+        }
+
+        $pciDevices = $esxcli.hardware.pci.list.Invoke() | Where-Object {$_.VMKernelName -like "vmhba*" -or $_.VMKernelName -like "vmnic*" -or $_.VMKernelName -like "vmgfx*"} | Sort-Object -Property VMKernelName 
+
+        foreach ($pciDevice in $pciDevices) {
+            $driverVersion = $esxcli.system.module.get.Invoke(@{module = $pciDevice.ModuleName}) | Select-Object -ExpandProperty Version
+
+            <#
+                Get NIC Firmware version
+            #>
+            if ($pciDevice.VMKernelName -like 'vmnic*') {
+                $vmnicDetail = $esxcli.network.nic.get.Invoke(@{nicname = $pciDevice.VMKernelName})
+                $firmwareVersion = $vmnicDetail.DriverInfo.FirmwareVersion
+                
+                <#
+                    Get NIC driver VIB package version
+                #>
+                $driverVib = $esxcli.software.vib.list.Invoke() | Select-Object -Property Name, Version | Where-Object {$_.Name -eq $vmnicDetail.DriverInfo.Driver -or $_.Name -eq "net-" + $vmnicDetail.DriverInfo.Driver -or $_.Name -eq "net55-" + $vmnicDetail.DriverInfo.Driver}
+                <#
+                    If HP Smart Array vmhba* (scsi-hpsa driver) then get Firmware version
+                    else skip if VMkernnel is vmhba*. Can't get HBA Firmware from 
+                    Powercli at the moment only through SSH or using Putty Plink+PowerCli.
+                #>
+            }
+            elseif ($pciDevice.VMKernelName -like 'vmhba*') {
+                if ($pciDevice.DeviceName -match "smart array") {
+                    $hpsa = $vmhost.ExtensionData.Runtime.HealthSystemRuntime.SystemHealthInfo.NumericSensorInfo | Where-Object {$_.Name -match "HP Smart Array"}
+                    if ($hpsa) {
+                        $firmwareVersion = (($hpsa.Name -split "firmware")[1]).Trim()
+                    }
+                }
+                <#
+                    Get HBA driver VIB package version
+                #>
+                $vibName = $pciDevice.ModuleName -replace "_", "-"
+                $driverVib = $esxcli.software.vib.list.Invoke() | Select-Object -Property Name, Version | Where-Object {$_.Name -eq "scsi-" + $VibName -or $_.Name -eq "sata-" + $VibName -or $_.Name -eq $VibName}
+            }
+            <#
+                Output collected data
+            #>
+            [PSCustomObject]@{
+                'VMkernel Name'    = $pciDevice.VMKernelName
+                'Device Name'      = $pciDevice.DeviceName
+                'Driver'           = $pciDevice.ModuleName
+                'Driver Version'   = $driverVersion
+                'Firmware Version' = $firmwareVersion
+                'VIB Name'         = $driverVib.Name
+                'VIB Version'      = $driverVib.Version
+            } 
+        } 
+    }
+
+    End{}
+    
+}
 #endregion Script Functions
 
 #region Script Body
@@ -909,6 +1009,13 @@ foreach ($VIServer in $Target) {
                                         $VMhostPciDevices | Table -Name "$VMhost PCI Devices" 
                                     }
                                     #endregion ESXi Host PCI Devices
+
+                                    #region ESXi Host PCI Devices Drivers & Firmware
+                                    Section -Style Heading5 'PCI Devices Drivers & Firmware' {
+                                        $VMhostPciDevicesDetails = Get-PciDeviceDetail -Server $vCenter -esxcli $esxcli | Sort-Object 'VMkernel Name' 
+                                        $VMhostPciDevicesDetails | Table -Name "$VMhost PCI Devices Drivers & Firmware" 
+                                    }
+                                    #endregion ESXi Host PCI Devices Drivers & Firmware
                                 }
 
                                 #region ESXi Host System Section
@@ -1082,9 +1189,9 @@ foreach ($VIServer in $Target) {
                                         "network configuration of $VMhost.")
                                     BlankLine
                                     #region ESXi Host Network Configuration
-                                    $VMHostNetwork = $VMhost | Get-VMHostNetwork | Select-Object  VMHost, @{L = 'Virtual Switches'; E = {($_.VirtualSwitch) -join ", "}}, @{L = 'VMKernel Adapters'; E = {($_.VirtualNic) -join ", "}}, 
-                                    @{L = 'Physical Adapters'; E = {($_.PhysicalNic) -join ", "}}, @{L = 'VMKernel Gateway'; E = {$_.VMKernelGateway}}, @{L = 'IPv6 Enabled'; E = {$_.IPv6Enabled}}, 
-                                    @{L = 'VMKernel IPv6 Gateway'; E = {$_.VMKernelV6Gateway}}, @{L = 'DNS Servers'; E = {($_.DnsAddress) -join ", "}}, @{L = 'Host Name'; E = {$_.HostName}}, 
+                                    $VMHostNetwork = $VMhost | Get-VMHostNetwork | Select-Object  VMHost, @{L = 'Virtual Switches'; E = {($_.VirtualSwitch) -join ", "}}, @{L = 'VMkernel Adapters'; E = {($_.VirtualNic) -join ", "}}, 
+                                    @{L = 'Physical Adapters'; E = {($_.PhysicalNic) -join ", "}}, @{L = 'VMkernel Gateway'; E = {$_.VMKernelGateway}}, @{L = 'IPv6 Enabled'; E = {$_.IPv6Enabled}}, 
+                                    @{L = 'VMkernel IPv6 Gateway'; E = {$_.VMKernelV6Gateway}}, @{L = 'DNS Servers'; E = {($_.DnsAddress) -join ", "}}, @{L = 'Host Name'; E = {$_.HostName}}, 
                                     @{L = 'Domain Name'; E = {$_.DomainName}}, @{L = 'Search Domain'; E = {($_.SearchDomain) -join ", "}}
                                     if ($Healthcheck.VMHost.IPv6Enabled) {
                                         $VMHostNetwork | Where-Object {$_.'IPv6 Enabled' -eq $false} | Set-Style -Style Warning -Property 'IPv6 Enabled'
