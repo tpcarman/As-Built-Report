042833887640e12e3c9cd5f7fd07f2c0c1474c3b
Tim Carman <tpcarman@gmail.com>
2018-11-12 20:40:10 +1100

    Fix vSAN Host Count and add PCI Device Drivers & Firmware

--- Reports/vSphere/vSphere.ps1
+++ Reports/vSphere/vSphere.ps1
@@ -455,6 +455,97 @@ function Get-ScsiDeviceDetail {
         'CapacityGB' = $CapacityGB
     }
 }
+
+Function Get-PciDeviceDetail {
+    <#
+    .SYNOPSIS
+    Helper function to return PCI Devices Drivers & Firmware information for a specific host.
+    .PARAMETER Server
+    vCenter VISession object.
+    .PARAMETER esxcli
+    Esxcli session object associated to the host.
+    .EXAMPLE
+    $Credentials = Get-Crendentials
+    $Server = Connect-VIServer -Server vcenter01.example.com -Credentials $Credentials
+    $VMHost = Get-VMHost -Server $Server -Name esx01.example.com
+    $esxcli = Get-EsxCli -Server $Server -VMHost $VMhost -V2
+    Get-PciDeviceDetail -Server $vCenter -esxcli $esxcli
+    VMkernel Name    : vmhba0
+    Device Name      : Sunrise Point-LP AHCI Controller
+    Driver           : vmw_ahci
+    Driver Version   : 1.0.0-34vmw.650.0.14.5146846
+    Firmware Version : NA
+    VIB Name         : vmw-ahci
+    VIB Version      : 1.0.0-34vmw.650.0.14.5146846
+    .NOTES
+    Author: Erwan Quelin heavily based on the work of the vDocumentation team - https://github.com/arielsanchezmora/vDocumentation/blob/master/powershell/vDocumentation/Public/Get-ESXIODevice.ps1
+    #>
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory = $true)]
+        $Server,
+        [Parameter(Mandatory = $true)]
+        $esxcli
+    )
+    Begin {}
+    
+    Process {
+        # Set default results
+        $firmwareVersion = "N/A"
+        $vibName = "N/A"
+        $driverVib = @{
+            Name = "N/A"
+            Version = "N/A"
+        }
+        $pciDevices = $esxcli.hardware.pci.list.Invoke() | Where-Object {$_.VMKernelName -like "vmhba*" -or $_.VMKernelName -like "vmnic*" -or $_.VMKernelName -like "vmgfx*"} | Sort-Object -Property VMKernelName 
+        foreach ($pciDevice in $pciDevices) {
+            $driverVersion = $esxcli.system.module.get.Invoke(@{module = $pciDevice.ModuleName}) | Select-Object -ExpandProperty Version
+            <#
+                Get NIC Firmware version
+            #>
+            if ($pciDevice.VMKernelName -like 'vmnic*') {
+                $vmnicDetail = $esxcli.network.nic.get.Invoke(@{nicname = $pciDevice.VMKernelName})
+                $firmwareVersion = $vmnicDetail.DriverInfo.FirmwareVersion
+                
+                <#
+                    Get NIC driver VIB package version
+                #>
+                $driverVib = $esxcli.software.vib.list.Invoke() | Select-Object -Property Name, Version | Where-Object {$_.Name -eq $vmnicDetail.DriverInfo.Driver -or $_.Name -eq "net-" + $vmnicDetail.DriverInfo.Driver -or $_.Name -eq "net55-" + $vmnicDetail.DriverInfo.Driver}
+                <#
+                    If HP Smart Array vmhba* (scsi-hpsa driver) then get Firmware version
+                    else skip if VMkernnel is vmhba*. Can't get HBA Firmware from 
+                    Powercli at the moment only through SSH or using Putty Plink+PowerCli.
+                #>
+            } elseif ($pciDevice.VMKernelName -like 'vmhba*') {
+                if ($pciDevice.DeviceName -match "smart array") {
+                    $hpsa = $vmhost.ExtensionData.Runtime.HealthSystemRuntime.SystemHealthInfo.NumericSensorInfo | Where-Object {$_.Name -match "HP Smart Array"}
+                    if ($hpsa) {
+                        $firmwareVersion = (($hpsa.Name -split "firmware")[1]).Trim()
+                    }
+                }
+                <#
+                    Get HBA driver VIB package version
+                #>
+                $vibName = $pciDevice.ModuleName -replace "_", "-"
+                $driverVib = $esxcli.software.vib.list.Invoke() | Select-Object -Property Name, Version | Where-Object {$_.Name -eq "scsi-" + $VibName -or $_.Name -eq "sata-" + $VibName -or $_.Name -eq $VibName}
+            }
+            <#
+                Output collected data
+            #>
+            [PSCustomObject]@{
+                'VMkernel Name' = $pciDevice.VMKernelName
+                'Device Name' = $pciDevice.DeviceName
+                'Driver' = $pciDevice.ModuleName
+                'Driver Version' = $driverVersion
+                'Firmware Version' = $firmwareVersion
+                'VIB Name' = $driverVib.Name
+                'VIB Version' = $driverVib.Version
+            } 
+        } 
+    }
+    End {}
+    
+}
 #endregion Script Functions
 
 #region Script Body
@@ -954,6 +1045,13 @@ foreach ($VIServer in $Target) {
                                         $VMhostPciDevices | Table -Name "$VMhost PCI Devices" 
                                     }
                                     #endregion ESXi Host PCI Devices
+
+                                    #region ESXi Host PCI Devices Drivers & Firmware
+                                    Section -Style Heading5 'PCI Devices Drivers & Firmware' {
+                                        $VMhostPciDevicesDetails = Get-PciDeviceDetail -Server $vCenter -esxcli $esxcli | Sort-Object 'VMkernel Name' 
+                                        $VMhostPciDevicesDetails | Table -Name "$VMhost PCI Devices Drivers & Firmware" 
+                                    }
+                                    #endregion ESXi Host PCI Devices Drivers & Firmware
                                 }
 
                                 #region ESXi Host System Section
@@ -1531,7 +1629,7 @@ foreach ($VIServer in $Target) {
                                     'Id' = $VsanCluster.Id
                                     'Type' = $VsanClusterType
                                     'Stretched Cluster' = $VsanCluster.StretchedClusterEnabled
-                                    'Number of Hosts' = ($VsanDiskGroup.VMHost).Count
+                                    'Number of Hosts' = $VsanCluster.Cluster.ExtensionData.Host.Count
                                     'Disk Format Version' = $VsanDiskFormat
                                     'Total Number of Disks' = $NumVsanDisk
                                     'Total Number of Disk Groups' = $NumVsanDiskGroup
